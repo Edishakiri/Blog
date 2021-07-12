8f3a1008da019289861bd79763e2fdff21117011
Robert Ścieszka <85113688+Edishakiri@users.noreply.github.com>
2021-07-12 20:12:25 +0200

    Create Singlepost.jsx

---
+++ scr/components/singlePost/Singlepost.jsx
@@ -0,0 +1,39 @@
+import "./singlepost.css"
+
+export default function Singlepost() {
+    return (
+        <div className="Singlepost">
+            <div className="singlePostWrapper">
+                <h1 className="singlePostTitle">Przemysł kawowy w Kolumbii
+                    
+                </h1>
+                <img className="singlePostImg" src="https://egsoyat786n.exactdn.com/wp-content/uploads/2019/06/coffee-renaissance-in-colombia.jpg?strip=all&lossy=1&w=616&ssl=1" alt="" />
+                    <div className="singlePostEdit">
+                        <i className="singlePostIcon fas fa-edit"></i>
+                        <i className="singlePostIcon fas fa-trash-alt"></i>
+                        
+                    </div>
+                     
+                <div className="singlePostInfo">
+                    <span className="singlePostAutor">Autor: <b>Robert Ścieszka</b></span>
+                   
+                   
+                </div>   
+                
+                 <p className="singlePostDesc">Prawdopodobnie nie jest zaskoczeniem, że przemysł kawowy w Kolumbii to dynamicznie rozwijający się mega biznes. Mogą nie być największym producentem kawy na świecie z zaledwie 12% produkcji, co plasuje ich w tyle za innymi czołowymi regionami uprawy kawy, takimi jak Wietnam i Brazylia ale to, w czym Kolumbia przoduje i wyróżnia ją na tle konkurencji, to ekskluzywne ziarna kawy Arabica z najwyższej półki.
+
+                       Prawie wszyscy mieszkańcy Kolumbii mają powiązania z uprawą kawy, a mając ponad pół miliona rolników, łatwo zrozumieć, dlaczego. W rzeczywistości jest to największe w kraju źródło zatrudnienia na wsi. Jeśli tego nie robią, najprawdopodobniej mają przyjaciela lub krewnego, który jest w jakiś sposób związany z kawą.
+
+                       Przy tak dużej ilości kawy uprawianej w tym regionie można by pomyśleć, że jest ona produkowana na dużą skalę przez „mega” korporacje. Nie. Praktycznie wszystkie kolumbijskie fasole są uprawiane na małej, rodzinnej ziemi o średniej powierzchni około 5 akrów.
+
+                       Małe działki pod uprawę kawy istnieją od pokoleń, z których każda jest własnością jednej rodziny. Jednak to wszystko może się zmienić z powodu ciągle zmieniających się wzorców pogodowych spowodowanych zmianą klimatu.
+ 
+                       Światowe zjawisko zmian klimatycznych zagraża uprawie kawy w Kolumbii. W rzeczywistości jest to jedno z największych zagrożeń dla całej branży, nie tylko w Kolumbii.
+
+                       Rosnące temperatury i nieprzewidywalne opady miały negatywny wpływ na produkcję ziaren kawy. Niestety ziarna Arabiki są szczególnie podatne na zmiany klimatyczne, a ponieważ ziarno to stanowi zdecydowaną większość produkcji w Kolumbii, przyszłość wygląda ponuro.</p>
+                    
+            </div>
+            <span className="singlePostData">Dodano: Godzinę temu</span> 
+        </div>
+    )
+}
